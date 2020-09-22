7fa0f04201f5c222e1ef4967dee2f5ba88b5b3e7
Igor Osadchyi <igor@webio.net.ua>
2020-09-22 20:37:07 +0300

    feat: implement createDreamTeam fn

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) return false;
+  return arr.map(x => (typeof x !== 'string') ? ('') : (x.trim().split('')[0].toUpperCase())).sort().join('');
 };
