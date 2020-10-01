8c30cbf13158bf09e8182db4ed96b420f7d6bbe0
Igor Osadchyi <igor@webio.net.ua>
2020-10-01 19:18:02 +0300

    feat: implement chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(String(value) || '');
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const posInt = parseInt(position);
+
+    if (isNaN(posInt) || posInt > this.getLength() || posInt < 0) {
+      this.chain = [];
+      throw new Error('Wrong position!');
+    }
+
+    this.chain.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const result = `( ${this.chain.join(' )~~( ')} )`;
+    this.chain = [];
+    return result;
   }
 };
 
