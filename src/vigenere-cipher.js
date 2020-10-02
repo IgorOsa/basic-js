84eb97ffdf2b68a3f632b13f1ecaefefa6ea3eff
Igor Osadchyi <igor@webio.net.ua>
2020-10-02 16:34:45 +0300

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.CHAR_ENCODE = 130;
+    this.CHAR_DECODE = 104;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw Error('provide all params!');
+
+    const m = message.toUpperCase();
+    const k = key.toUpperCase();
+    const result = [];
+
+    let step = 0;
+
+    for (const i in m) {
+      if (m[i].match(/[A-Z]/)) {
+        result.push(this.alphabet.charAt((m[i].charCodeAt() +
+          (k[step % k.length]).charCodeAt() - this.CHAR_ENCODE) % this.alphabet.length));
+        step++;
+      } else {
+        result.push(m[i]);
+      }
+    }
+
+    return !this.direct ? result.reverse().join('') : result.join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw Error('provide all params!');
+
+    const m = message.toUpperCase();
+    const k = key.toUpperCase();
+    const result = [];
+
+    let step = 0;
+
+    for (const i in m) {
+      if (m[i].match(/[A-Z]/)) {
+        result.push(this.alphabet.charAt((m[i].charCodeAt() -
+          (k[step % k.length]).charCodeAt() + this.CHAR_DECODE) % this.alphabet.length));
+        step++;
+      } else {
+        result.push(m[i]);
+      }
+    }
+
+    return !this.direct ? result.reverse().join('') : result.join('');
   }
 }
 
