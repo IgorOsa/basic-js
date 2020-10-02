9c414c0d47f241c70d0464e193f2a838316646ee
Igor Osadchyi <igor@webio.net.ua>
2020-10-02 09:43:38 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = [];
+
+  for (let s = 0; s < (options["repeatTimes"] || 1); s++) {
+    let addition = [];
+    let resultStr = '';
+
+    if ("addition" in options) {
+      for (let i = 0; i < (options["additionRepeatTimes"] || 1); i++) {
+        addition.push(String(options.addition));
+      }
+      resultStr = addition.join((options.additionSeparator || '|'));
+    }
+
+    result.push(String(str) + resultStr);
+  }
+  return result.join((options.separator || '+'))
 };
-  
+
+module.exports = repeater;
